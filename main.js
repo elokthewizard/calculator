a5f15dfe1c5b07d3e07820ad76bc4da9e15dcdc1
elokthewizard <148482234+elokthewizard@users.noreply.github.com>
2023-11-25 11:17:31 -0700

    create operate function & display storage variables

--- main.js
+++ main.js
@@ -13,3 +13,11 @@ const multiply = function(a,b) {
 const divide = function(a,b) {
     return (a/b)
 };
+
+const displayOne = null
+const displayTwo = null
+const displayOp = null
+
+const operate = function(firstOne, secondOne, operator) {
+    return operator(firstOne, secondOne)
+}
