7758e9b22593bdcc805e710aec78243ac61e0e70
elokthewizard <148482234+elokthewizard@users.noreply.github.com>
2023-11-25 11:07:56 -0700

    write divide function

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-const add = function(a, b) {
+const add = function(a,b) {
     return (a + b);
 };
 
-const subtract = function(a, b) {
-    return (a -b);
+const subtract = function(a,b) {
+    return (a - b);
 };
 
-const multiply = function(a, b) {
+const multiply = function(a,b) {
     return (a * b)
-}
+};
+
+const divide = function(a,b) {
+    return (a/b)
+};
