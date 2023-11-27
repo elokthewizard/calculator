89466e9f92509e40b5c33934dbf39684975a8dcb
elokthewizard <148482234+elokthewizard@users.noreply.github.com>
2023-11-27 14:08:04 -0700

    show button selection in display div

--- main.js
+++ main.js
@@ -14,10 +14,18 @@ const divide = function(a,b) {
     return (a/b)
 };
 
-const displayOne = null
-const displayTwo = null
-const displayOp = null
+const calculator = document.querySelector('#Calculator');
+const input = document.querySelector('input');
+const button = document.querySelector('button')
 
-const operate = function(firstOne, secondOne, operator) {
-    return operator(firstOne, secondOne)
-}
+calculator.addEventListener('click', e => {
+    input.value += e.target.textContent;
+});
+
+// const operand1 = null
+// const operand2 = null
+// const operator = null
+
+// const operate = function(firstOne, secondOne, operator) {
+//     return (firstOne, operator, secondOne)
+// }
