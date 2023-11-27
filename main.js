706db1f1ab97cec43a7f901f1629803a6ef8b8aa
elokthewizard <148482234+elokthewizard@users.noreply.github.com>
2023-11-27 14:25:36 -0700

    make result change display and accept any operator

--- main.js
+++ main.js
@@ -19,8 +19,41 @@ const input = document.querySelector('input');
 const button = document.querySelector('button')
 
 calculator.addEventListener('click', e => {
+
     input.value += e.target.textContent;
+
+    if (e.target.id == "equals"){
+        let expression = input.value
+        let elements = expression.split(/(\D)/);
+        
+        let operand1 = parseInt(elements[0]);
+        let operand2 = parseInt(elements[2]);
+        let operator = elements[1];
+    
+        let result;
+    
+        switch (operator) {
+            case '+':
+                result = add(operand1,operand2)
+                break;
+            case '-':
+                result = subtract(operand1,operand2)
+                break;
+            case '*':
+                result = multiply(operand1,operand2)
+                break;
+            case "/":
+                result = divide(operand1,operand2)
+                break;
+            default:
+                console.error("Invalid Operator")
+        }
+        input.value = result;
+    
+    };
 });
+
+
 
 // const operand1 = null
 // const operand2 = null
