ffdb95280b7ba391c725e19bf45642d1a42c8368
elokthewizard <148482234+elokthewizard@users.noreply.github.com>
2023-11-28 12:26:23 -0700

    initilize variables
    rewrite was neccessar afterall

---
+++ main2.js
@@ -0,0 +1,10 @@
+let firstOperand = '';
+let secondOperand = '';
+let operator = '';
+let result = '';
+let lastResult = '';
+let operands = [];
+
+let operatorWasClicked = false;
+let lastClickWasEquals = false;
+
