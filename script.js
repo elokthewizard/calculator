0327a662fe64b1a6555e659ed338294e9d91bf52
elokthewizard <148482234+elokthewizard@users.noreply.github.com>
2023-11-29 12:33:35 -0700

    add numebr at button pushed to display string

--- script.js
+++ script.js
@@ -14,5 +14,25 @@ const divide = function(a,b) {
     return (a / b)
 };
 
+let operand1 = '';
+let operand2 = '';
+let operator1 = '';
+let operator2 = '';
+let result = '';
+let lastResult = '';
+
 const calculator = document.querySelector('#Calculator');
 const display = document.querySelector('input');
+
+calculator.addEventListener('click', handleClick);
+
+function handleClick(e) {
+    const buttonClicked = e.target;
+    if (buttonClicked.tagName !== 'BUTTON') {
+        return; // if we clicked a non button, return nothing
+    }; 
+    const buttonText = buttonClicked.textContent;
+    if (buttonClicked.className === "number") {
+        display.value += buttonText
+    }
+}
