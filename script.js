ef68318e49e621a018379b346b905384bda1d1b9
elokthewizard <148482234+elokthewizard@users.noreply.github.com>
2023-11-29 12:50:11 -0700

    handle second operator assignment and execution of calc function

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ const divide = function(a,b) {
     return (a / b)
 };
 
-let operand1 = '';
-let operand2 = '';
+let firstOperand = '';
+let secondOperand = '';
 let operator1 = '';
 let operator2 = '';
 let result = '';
@@ -37,11 +37,43 @@ function handleClick(e) {
     }
     if (buttonClicked.className === 'operator' && display.value !== '') {
         operator1 = buttonText;
-        operand1 = display.value;
+        firstOperand = display.value;
         clearDisplay();
+    }
+    if (buttonClicked.id === 'equals') {
+        if (firstOperand !== '' && operator1 !== '') {
+            secondOperand = display.value;
+            operate();
+        }
     }
 }
 
 function clearDisplay() {
     display.value = '';
+}
+
+function operate() {
+    firstOperand = parseFloat(firstOperand);
+    secondOperand = parseFloat(secondOperand);
+
+    switch (operator1.toString()) {
+        case '+':
+            result = add(firstOperand,secondOperand)
+            break;
+        case '-':
+            result = subtract(firstOperand,secondOperand)
+            break;
+        case '*':
+            result = multiply(firstOperand,secondOperand)
+            break;
+        case '/':
+            result = divide(firstOperand,secondOperand)
+            break;
+        default:
+            console.error('Invalid operator')
+            return;
+    }
+    lastResult = result
+    clearDisplay()
+    display.value = result;
 }
